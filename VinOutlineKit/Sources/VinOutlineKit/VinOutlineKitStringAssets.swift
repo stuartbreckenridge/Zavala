01ea9d1c559755b465a06251279c33bb2b3d03ec
Stuart Breckenridge <stuart.breckenridge@icloud.com>
2025-01-19 20:33:50 +0800

    🌐 VinOutlineKit uses module for localizations

--- VinOutlineKit/Sources/VinOutlineKit/VinOutlineKitStringAssets.swift
+++ VinOutlineKit/Sources/VinOutlineKit/VinOutlineKitStringAssets.swift
@@ -8,25 +8,25 @@ import Foundation
 
 struct VinOutlineKitStringAssets {
 	
-	static let accountOnMyMac = String(localized: "label.text.on-my-mac", comment: "Local Account Name: On My Mac")
-	static let accountOnMyIPad = String(localized: "label.text.on-my-ipad", comment: "Local Account Name: On My iPad")
-	static let accountOnMyIPhone = String(localized: "label.text.on-my-iphone", comment: "Local Account Name: On My iPhone")
-	static let accountICloud = String(localized: "iCloud", comment: "iCloud Account Name: iCloud")
+    static let accountOnMyMac = String(localized: "label.text.on-my-mac", bundle: .module, comment: "Local Account Name: On My Mac")
+	static let accountOnMyIPad = String(localized: "label.text.on-my-ipad", bundle: .module, comment: "Local Account Name: On My iPad")
+	static let accountOnMyIPhone = String(localized: "label.text.on-my-iphone", bundle: .module, comment: "Local Account Name: On My iPhone")
+	static let accountICloud = String(localized: "iCloud", bundle: .module, comment: "iCloud Account Name: iCloud")
 
-	static let noTitle = String(localized: "label.text.no-title", comment: "Label: (No Title)")
-	static let all = String(localized: "label.text.all", comment: "Label: All")
-	static let search = String(localized: "label.text.search", comment: "Label: Search")
+	static let noTitle = String(localized: "label.text.no-title", bundle: .module, comment: "Label: (No Title)")
+	static let all = String(localized: "label.text.all", bundle: .module, comment: "Label: All")
+	static let search = String(localized: "label.text.search", bundle: .module,  comment: "Label: Search")
 
 	static let accountErrorImportRead = String(localized: "label.text.unable-to-read-import-file",
-											   comment: "Label: Unable to read the import file.")
+                                               bundle: .module, comment: "Label: Unable to read the import file.")
 	static let accountErrorOPMLParse = String(localized: "label.text.unable-to-process-opml",
-											   comment: "Label: Unable to process the OPML data.")
+                                              bundle: .module, comment: "Label: Unable to process the OPML data.")
 	static let accountErrorRenameTagExists = String(localized: "label.text.tag-already-exists",
-												   comment: "Label: This Tag name already exists. Please choose a different name.")
+                                                    bundle: .module, comment: "Label: This Tag name already exists. Please choose a different name.")
 	static let accountErrorScopedResource =	String(localized: "label.text.unable-to-access-security-scoped-resoruces",
-												   comment: "Label: Unable to access security scoped resource.")
+                                                   bundle: .module, comment: "Label: Unable to access security scoped resource.")
 
 	static let rowDeserializationError = String(localized: "label.text.unable-to-deserialize-row-data",
-												comment: "Label: Unable to deserialize the row data.")
+                                                bundle: .module, comment: "Label: Unable to deserialize the row data.")
 
 }
